f8cc059ed1cc07ec3a4c172e015f15690a81e654
Victor Roberth Gomes Said <victor.said@tce.ma.gov.br>
2025-02-13 11:15:56 -0300

    logo name

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,7 +33,7 @@ class AdminPanelProvider extends PanelProvider
             ->profile()
             ->passwordReset()
             ->sidebarCollapsibleOnDesktop()
-            ->brandLogo(asset('storage/logo/Logo_astce.jpeg'))
+            ->brandLogo(asset('storage/logo/logo_astce.jpeg'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
