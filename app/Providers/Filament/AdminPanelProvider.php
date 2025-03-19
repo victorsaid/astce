bd60c4bb818339500218aa7f87b0319eeb8c61bd
victor <victorsaid@acad.ifima.edu.br>
2025-03-18 22:46:03 -0300

    logo dark

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -41,6 +41,7 @@ class AdminPanelProvider extends PanelProvider
             ->passwordReset()
             ->sidebarCollapsibleOnDesktop()
             ->brandLogo(asset('storage/logo/logo_astce.jpeg'))
+            ->darkModeBrandLogo(asset('storage/logo/logo_astce_darkmode.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
